2743f6bd12829301430216c6e2021a1bdf9bdbde
Amrit Anand <64958314+amritanand-py@users.noreply.github.com>
2023-03-30 13:10:34 +0530

    Camera Preview
    Live Camera Preview has been added for live Translation along with few  basic Positional changes in Button and Label in Secondary Screen of Reverse Translation

--- TalkingSigns/backwardsTranslateViewController.swift
+++ TalkingSigns/backwardsTranslateViewController.swift
@@ -8,14 +8,20 @@ import PhotosUI
 import MessageUI
 import SafariServices
 import UIKit
+import AVFoundation
 
-class backwardsTranslateViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+class backwardsTranslateViewController: UIViewController,AVCapturePhotoCaptureDelegate ,UIImagePickerControllerDelegate & UINavigationControllerDelegate {
     
-    
+
     @IBOutlet weak var galleryButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .black
+        view.layer.addSublayer(previewLayer)
+        //view.addSubview(shutterButton)
+        checkCameraPermissions()
     }
     
     
@@ -46,26 +52,115 @@ class backwardsTranslateViewController: UIViewController, UIImagePickerControlle
     
     
     
-    @IBOutlet weak var imageView: UIImageView!
+//    @IBOutlet weak var imageView: UIImageView!
+//
+//
+//    @IBAction func shareButtonTapped(_ sender: UIButton ) {
+//
+//
+//        guard let image = imageView.image else {
+//            return
+//        }
+//
+//        let activityController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+//
+//        activityController.popoverPresentationController?.sourceView = sender
+//
+//        present(activityController, animated: true, completion: nil)
+//
+//        if let url = URL(string: "https://www.apple.com") {
+//            let safariViewController = SFSafariViewController(url: url)
+//
+//            present(safariViewController, animated: true, completion: nil)
+//        }
     
     
-    @IBAction func shareButtonTapped(_ sender: UIButton ) {
+    @IBOutlet weak var screen: UIView!
+    
+    var captureSession = AVCaptureSession?.self
+    let output = AVCapturePhotoOutput()
+    var captureDevice: AVCaptureDevice?
+    var captureDeviceInput: AVCaptureDeviceInput?
+    let previewLayer = AVCaptureVideoPreviewLayer()
+    var session = AVCaptureSession()
+    
+    enum CameraCase {
+        case front
+        case back
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        previewLayer.frame = screen.bounds
         
-        
-        guard let image = imageView.image else {
-            return
-        }
-        
-        let activityController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
-        
-        activityController.popoverPresentationController?.sourceView = sender
-        
-        present(activityController, animated: true, completion: nil)
-        
-        if let url = URL(string: "https://www.apple.com") {
-            let safariViewController = SFSafariViewController(url: url)
-            
-            present(safariViewController, animated: true, completion: nil)
+//        shutterButton.center = CGPoint(x: screen.frame.size.width/2, y: screen.frame.size.height - 100)
+    }
+    
+    private func checkCameraPermissions() {
+        switch AVCaptureDevice.authorizationStatus(for: .video){
+        case .notDetermined:
+            AVCaptureDevice.requestAccess(for: .video) {
+                granted in guard granted else {
+                    return
+                }
+                DispatchQueue.main.async {
+                    self.setUpCamera()
+                }
+            }
+        case .restricted:
+            break
+        case .denied:
+            break
+        case .authorized:
+            setUpCamera()
+        @unknown default:
+            break
         }
     }
+    
+    private func setUpCamera() {
+        
+        if let device = AVCaptureDevice.default(for: .video){
+            do {
+                let input = try AVCaptureDeviceInput(device: device)
+                if session.canAddInput(input){
+                    session.addInput(input)
+                }
+                if session.canAddOutput(output){
+                    session.addOutput(output)
+                }
+                
+                previewLayer.videoGravity = .resizeAspectFill
+                previewLayer.session = session
+                
+                session.startRunning()
+                //self.session = session
+            }
+            catch{
+                print(error)
+                
+            }
+        }
+    }
+    
+    @objc private func didTapTakePhoto() {
+        output.capturePhoto(with: AVCapturePhotoSettings(), delegate: self)
+    }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        guard let data = photo.fileDataRepresentation() else{
+            return
+        }
+        let image = UIImage(data: data)
+        session.stopRunning()
+        
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFill
+        imageView.frame = view.bounds
+        view.addSubview(imageView)
+    }
 }
+
+    
+
+
